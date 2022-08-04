89cd5acf1d9a77f4768447cc88c55a177f7ffd94
Juampi095 <azarjpm15@gmail.com>
2022-08-04 16:12:46 -0300

    agregue una funcion de saludo

--- codigoJS.js
+++ codigoJS.js
@@ -1,30 +1,42 @@
-// //Marca de Autos
-let marcaAutos = prompt("Ingresa la marca y te diremos el precio y la disponibilidad");
-switch (marcaAutos) {
-    case "lamborghini":
-    case "LAMBORGHINI":
-
-        alert("El precio es de $12499 y tenemos en stock");
-        break;
-
-    case "pagani":
-
-    case "PAGANI":
-
-        alert("El precio es de $14999 y tenemos en stock");
-
-        break;
-
-    case "ferrari":
-    case "FERRARI":
-        alert("El precio es de $12999 y tenemos en stock");
-        break;
-
-    default:
-        alert("No tenemos en stock por el momento, revisa mas tarde");
-        break;
+//Saludo
+function saludar() {
+    let nombre = prompt("Cual es tu nombre?");
+    alert("¡Bienvenido a CarArt! " + nombre);
 }
+saludar();
 
+// //Marca de Autos
+
+
+function marcaDeAutos() {
+
+    let marcaAutos = prompt("Ingresa la marca y te diremos el precio y la disponibilidad");
+    switch (marcaAutos) {
+        case "lamborghini":
+        case "LAMBORGHINI":
+
+            alert("El precio es de $12499 y tenemos en stock");
+            break;
+
+        case "pagani":
+
+        case "PAGANI":
+
+            alert("El precio es de $14999 y tenemos en stock");
+
+            break;
+
+        case "ferrari":
+        case "FERRARI":
+            alert("El precio es de $12999 y tenemos en stock");
+            break;
+
+        default:
+            alert("No tenemos en stock por el momento, revisa mas tarde");
+            break;
+    }
+}
+marcaDeAutos();
 
 
 
